a04352adc2cae82082bb173f4b81167717954763
kael38938-sys <kael38938@gmail.com>
2026-05-04 10:39:32 +0530

    Delete app/src/main/cpp/EmuCore.kt

--- app/src/main/cpp/EmuCore.kt
+++
@@ -1,9 +0,0 @@
-package com.moonlight.moongba
-
-object EmuCore {
-    init { System.loadLibrary("gba_core") }
-
-    @JvmStatic external fun nativeInit(): Boolean
-    @JvmStatic external fun nativeLoadRom(rom: ByteArray): Boolean
-    @JvmStatic external fun nativeStepFrame(): ByteArray
-}
